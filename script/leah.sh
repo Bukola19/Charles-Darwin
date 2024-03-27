4ed2278d1303df74f14cf52e7e7fc09180282598
LeahIkubanni <163176382+LeahIkubanni@users.noreply.github.com>
2024-03-27 09:59:58 +0100

    Add files via upload

---
+++ script/leah.sh
@@ -0,0 +1,285 @@
+    1  mkdir Leah
+    2  mkdir biocomputing && cd biocomputing
+    3  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+    4  mv *.fna ~/Leah/
+    5  rm wildtype.gbk.1
+    6  grep -o 'tata' wildtype.fna 
+    7  grep -o 'tatatata' wildtype.fna
+    8  ls
+    9  grep -o 'tatatata' wildtype.fna
+   10  mkdir leah
+   11  mkdir biocomputing ; cd biocomputing
+   12  wget https://raw.githubusercontent.com/jisoga2/dataset-repos/main/wildtype.fna https://raw.githubusercontent.com/jisoga2/dataset-repos/main/wildtype.gbk https://raw.githubusercontent.com/jisoga2/dataset-repos/main/wildtype.gbk
+   13  
+   14  wget https://raw.githubusercontent.com/jisoga2/dataset-repos/main/wildtype.fna https://raw.githubusercontent.com/jisoga2/dataset-repos/main/wildtype.gbk https://raw.githubusercontent.com/jisoga2/dataset-repos/main/wildtype.gbk
+   15  wget https://raw.githubusercontent.com/jisoga/dataset-repos/main/wildtype.fna https://raw.githubusercontent.com/jisoga2/dataset-repos/main/wildtype.gbk https://raw.githubusercontent.com/jisoga2/dataset-repos/main/wildtype.gbk
+   16  wget https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.fna https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk https://raw.githubusercontent.com/josoga2/dataset-repos/main/wildtype.gbk
+   17  mv *.fna ../leah/
+   18  rm 'wildtype.gbk.1
+   19  ls
+   20   $ grep "tatatata".fna > mutant_lines.txt
+   21  grep "tatatata".fna > mutant_lines.txt
+   22  grep -o 'tatatata' wildtype.fna
+   23  leah textfile.txt
+   24  ls
+   25  pwd
+   26  /leah/
+   27  ./leah.sh
+   28  cd /leah/
+   29  >cd /leah/
+   30  ~/leah?
+   31  ~/leah/
+   32  ls
+   33  cd ~/leah
+   34  grep -o 'tatatata'wildtype.fna
+   35  grep -o 'tata' wildlife.fna
+   36  grep 'tatatata' wildtype.fna >mutant_lines.txt
+   37  esearch -db nucleotide -id "NC_000007.14" -format fasta >NC_000007.14. fasta'
+   38  ls
+   39  wget -O FOXP2.fasta "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?db=fasta&NC_000007.14=NC_000007.14
+   40  cd ~/leah
+   41  wget https:eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000007.14
+   42  wget 
+   43  wget https:www.ncbi.nlm.nih.gov/nuccore/NC_000007.14?report=fasta&from=114086327&to=114693765#
+   44  wget FOXP2.fasta "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?db=nuccore&report=fasta&id=NC_000007.14
+   45  wget "https://www.ncbi.nlm.nih.gov/nuccore/NC_000007.14?report=fasta&from=114086327&to=114693765#"
+   46  ls
+   47  
+   48  {nano}
+   49  {Nano}
+   50  cd ~/leah
+   51  ls
+   52  tail -n +2 FOXP2_Geneseq.fasta
+   53  wget "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000007.14&rettype=fasta&retmode=text" -OFOXP2.fasta
+   54  grep ">" FOXP2.fasta | wc -l
+   55  grep -v ">" FOXP2.fasta | wc -l
+   56  tail -n +2 FOXP2.fasta | grep -o -i "A" | wc -l
+   57  tail -n +2 FOXP2.fasta | grep -o -i "G" | wc -l
+   58  tail -n +2 FOXP2.fasta | grep -o -i "C" | wc -l
+   59  tail -n +2 FOXP2.fasta | grep -o -i "T" | wc -l
+   60  grep -v '>' FOXP2.fasta | wc -c
+   61  grep -v '>' FOXP2 | tr -dc 'A' | wc -c
+   62  grep -v '>' FOXP2.fasta | tr -dc 'A' |wc -c
+   63  grep -v '>' FOXP2.fasta | tr -dc 'G' | wc -c
+   64  grep -v '>' FOXP2.fasta | tr -dc 'A' | wc -c
+   65  grep -v '>' FOXP2.fasta | tr -dc 'C' | wc -c
+   66  grep -v '>' FOXP2.fasta | tr -dc 'T' | wc -c
+   67  echo "($(grep -v '>' FOXP2.fasta | tr -dc 'G' | wc -c) + $(grep -v '>' FOXP2.fasta | tr -dc 'C'))/ $(grep -v '>' FOXP2.fasta | wc -c) * 100 "| bc
+   68  ls
+   69  echo "($(grep -v '>' FOXP2.fasta | tr -dc 'G'| wc -c) + $(grep -v '>' FOXP2.fasta | tr -dc 'C'| wc -c))/ $(grep -v '>' FOXP2.fasta | wc -c) * 100 " |bc 
+   70  grep -v '>' FOXP2.fasta | wc -c
+   71  grep -v '>' FOXP2.fasta | tr -dc 'G'| wc -c
+   72  grep -v '>' FOXP2.fasta | tr -dc 'C' | wc -c
+   73  grep -v '>' FOXP2.fasta | tr -dc 'A' | wc -c
+   74  grep -v '>' FOXP2.fasta | tr -dc 'T' | wc -c
+   75  echo "($(grep -v '>' FOXP2.fasta | tr -dc 'G'| wc -c) + $(grep -v '>' FOXP2.fasta |tr -dc 'C'| wc -c))/ $(grep -v '>' FOXP2.fasta | wc -c) * 100 " |bc
+   76  echo "($(grep -v '>'FOXP2.fasta | tr -dc 'G'| wc -c) + $(grep -v '>' FOXP2.fasta | tr -dc 'C'| wc -c)) / $(grep -v '>' FOXP2.fasta | wc -c) * 100" | bc
+   77  wget FOXP2.fasta "https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?db=nuccore&report=fasta&id=NC_000007.14
+   78  wget "https://www.ncbi.nlm.nih.gov/nuccore/NC_000007.14?report=fasta&from=114086327&to=114693765#"
+   79  ls
+   80  
+   81  {nano}
+   82  {Nano}
+   83  cd ~/leah
+   84  ls
+   85  tail -n +2 FOXP2_Geneseq.fasta
+   86  wget "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000007.14&rettype=fasta&retmode=text" -OFOXP2.fasta
+   87  grep ">" FOXP2.fasta | wc -l
+   88  grep -v ">" FOXP2.fasta | wc -l
+   89  tail -n +2 FOXP2.fasta | grep -o -i "A" | wc -l
+   90  tail -n +2 FOXP2.fasta | grep -o -i "G" | wc -l
+   91  tail -n +2 FOXP2.fasta | grep -o -i "C" | wc -l
+   92  tail -n +2 FOXP2.fasta | grep -o -i "T" | wc -l
+   93  grep -v '>' FOXP2.fasta | wc -c
+   94  grep -v '>' FOXP2 | tr -dc 'A' | wc -c
+   95  grep -v '>' FOXP2.fasta | tr -dc 'A' |wc -c
+   96  grep -v '>' FOXP2.fasta | tr -dc 'G' | wc -c
+   97  grep -v '>' FOXP2.fasta | tr -dc 'A' | wc -c
+   98  grep -v '>' FOXP2.fasta | tr -dc 'C' | wc -c
+   99  grep -v '>' FOXP2.fasta | tr -dc 'T' | wc -c
+  100  echo "($(grep -v '>' FOXP2.fasta | tr -dc 'G' | wc -c) + $(grep -v '>' FOXP2.fasta | tr -dc 'C'))/ $(grep -v '>' FOXP2.fasta | wc -c) * 100 "| bc
+  101  ls
+  102  echo "($(grep -v '>' FOXP2.fasta | tr -dc 'G'| wc -c) + $(grep -v '>' FOXP2.fasta | tr -dc 'C'| wc -c))/ $(grep -v '>' FOXP2.fasta | wc -c) * 100 " |bc 
+  103  grep -v '>' FOXP2.fasta | wc -c
+  104  grep -v '>' FOXP2.fasta | tr -dc 'G'| wc -c
+  105  grep -v '>' FOXP2.fasta | tr -dc 'C' | wc -c
+  106  grep -v '>' FOXP2.fasta | tr -dc 'A' | wc -c
+  107  grep -v '>' FOXP2.fasta | tr -dc 'T' | wc -c
+  108  echo "($(grep -v '>' FOXP2.fasta | tr -dc 'G'| wc -c) + $(grep -v '>' FOXP2.fasta |tr -dc 'C'| wc -c))/ $(grep -v '>' FOXP2.fasta | wc -c) * 100 " |bc
+  109  echo "($(grep -v '>'FOXP2.fasta | tr -dc 'G'| wc -c) + $(grep -v '>' FOXP2.fasta | tr -dc 'C'| wc -c)) / $(grep -v '>' FOXP2.fasta | wc -c) * 100" | bc
+  110  echo "($(grep -v '>' FOXP2.fasta | tr -dc 'G'|wc -c) + $(grep -v '>' FOXP2.fasta | tr -dc 'C'| wc -c))/ $(grep -v '>' FOXP2.fasta | wc -c) * 100 |bc
+  111  echo "($(grep -v '>' FOXP2.fasta | tr -dc 'G'|wc -c) + $(grep -v '>' FOXP2.fasta | tr -dc 'C'| wc -c))/ $(grep -v '>' FOXP2.fasta | wc -c) * 100" |bc
+  112  touch leah.fasta
+  113  echo 'grep -v '>' FOXP2.fasta |tr -dc 'G'| wc -c" >> leah.fasta
+  114  $echo "grep -v '>' FOXP2.fasta | tr -dc 'C'| wc -c" >> leah.fasta
+  115  $echo "grep -v '>' FOXP2.fasta | tr -dc 'T'| wc -c" >> leah.fasta
+  116  $echo "grep -v '>' FOXP2.fasta |tr -dc 'A'| wc -c" >> leah .fasta
+  117  $echo "grep -v '>' FOXP2.fasta | tr -dc 'G'| wc -c >> leah . fasta
+  118  ls
+  119  cd ../leah.fasta
+  120  grep -o 'tatatata' wildtype.fna
+  121  touch leah.sh
+  122  history > leah.sh
+  123  echo "($(grep -v "^>" FOXP2.fasta | tr -cd 'G' | wc -c) + $(grep -v "^>" FOXP2.fasta |tr -cd 'C'| wc -c)) / $(grep -v'>' FOXP2.fasta | tr -dc 'ATGC'| wc -c) * 100 "|bc
+  124  cd ../leah
+  125  cd ~/leah
+  126  echo "($(grep -v "^>" FOXP2. fasta | tr -cd 'G' | wc -c) + $(grep -v "^>" FOXP2.fasta | tr -cd 'C')) / $(grep -v'>' FOXP2.fasta | tr -dc 'ATGC' | wc -c) * 100 " |bc
+  127  grep -v '>' FOXP2.fasta | tr -dc 'A' | wc -c
+  128  echo "($(grep -v "^>" FOXP2.fasta | tr -cd 'G' | wc -c) + $(grep -v "^>" FOXP2.fasta | tr -cd 'C')) / $(grep -v'>' FOXP2.fasta | tr -dc 'ATGC' |wc -c) * 100 " | bc
+  129  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+  130  ls
+  131  sh Miniconda3-latest-linux-x86_64.sh
+  132  bash /opt/miniconda-installer.sh
+  133  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe
+  134  bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda
+  135  mkdir -p ~/miniconda3
+  136  ls
+  137  pwd
+  138  cd ~/miniconda3
+  139  cd ~/leah
+  140  mv Miniconda3-latest-Linux-x86_64.sh >> miniconda3
+  141  mv 'Miniconda3-latest-Linux-x86_64.sh' >> miniconda3
+  142  cp 'Miniconda3-latest-Linux-x86_64.sh' >> ~/miniconda3 
+  143  cd ~miniconda3
+  144  cd ~/miniconda3
+  145  ls
+  146  cd ~/leah
+  147  mv 'Miniconda3-latest-Linux-x86_64.sh" >> ~/miniconda3
+  148  ls
+  149  ls
+  150  cd ~miconda3
+  151  cd ~/miniconda3
+  152  ls
+  153  wget <https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh> -O ~/miniconda3/miniconda.sh
+  154  bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+  155  sh "Miniconda3-latest-Linux-x86_64.sh" 
+  156  -u
+  157  source ~/.bashrc
+  158  sh "Miniconda3-latest-Linux-x86_64.sh"
+  159  cd ~/home
+  160  exit
+  161  wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
+  162  sha256sum Anaconda3-2022.05-Linux-x86_64.sh
+  163  bash Anaconda3-2022.05-Linux-x86_64.sh
+  164  conda install tsnyder:: figlet
+  165  condal install tsnyder::figlet
+  166  conda install tsnyder::figlet
+  167  figlet Leah
+  168  conda install -c biconda samtools
+  169  conda config --show channels
+  170  conda config --show-sources
+  171  conda install -c biconda spades.py
+  172  conda install -c bioconda multiqc
+  173  wget https://github.com/ablab/spades/releases/download/v3.15.5/SPAdes-3.15.5-Linux.tar.gz
+  174  conda config --add channels bioconda
+  175  conda config --add channels conda
+  176  conda install -c bioconda samtools
+  177  conda create -n samtools samtools
+  178  conda env list
+  179  bcf tools
+  180  conda activate funtools
+  181  bcf tools
+  182  samtools
+  183  bwa
+  184  figlet
+  185  blast
+  186  cd ~/leah
+  187  blast
+  188  cd ~/home
+  189  blast tools
+  190  blast
+  191  bedtools
+  192  figlet
+  193  conda env list
+  194  conda activate base
+  195  blast
+  196  bwa
+  197  figlet
+  198  figlet zoe
+  199  cd ~/anaconda3
+  200  blast
+  201  bwa
+  202  bcf
+  203  condal activate funtools
+  204  conda activate funtools
+  205  figlet
+  206  apt list --installed
+  207  -f
+  208  if test -f "leah.fasta" echo "leah.fasta" exists
+  209  cd ~/leah
+  210  ls
+  211  cat leah.fasta
+  212  cat 'leah.fasta'
+  213  cat FOXP2.fasta
+  214  clear
+  215  grep -v '>' FOXP2.fasta |tr -dc 'C'| wc -c
+  216  echo "grep -v '>' FOXP2.fasta |tr -dc 'C'| wc -c" >> leah.fasta
+  217  cat leah.fasta
+  218  history
+  219  grep -v '>' FOXP2.fasta |tr -dc 'T' | wc -c
+  220  echo "grep -v '>' FOXP2.fasta |tr -dc 'T' | wc -c >> leah.fasta
+  221  cat leah.fasta
+  222  echo "grep -v '>' FOXP2.fasta |tr -dc 'T' | wc -c" >> leah.fasta
+  223  cat leah.fasta
+  224  grep -v '>' FOXP2.fasta |tr -dc 'A' | wc -c
+  225  echo "number of A : 2389738" >> leah.fasta
+  226  cat leah.fasta
+  227  grep -v '>' FOXP2.fasta |tr -dc 'G' | wc -c
+  228  echo "number of G : 2161390 >> leah.fasta
+  229  echo "number of G : 2161390" >> leah.fasta
+  230  echo "number of T : 2407932" >> leah.fasta
+  231  grep -v '>' FOXP2 |tr -dc 'C' | wc -c
+  232  grep -v '>' FOXP2.fasta |tr -dc 'C' | wc -c
+  233  echo "number of C : 2162527" >> leah.fasta
+  234  cat leah.fasta
+  235  rm "grep -v '>' FOXP2.fasta |tr -dc 'C'| wc -c" >> leah.fasta
+  236  cat leah.sh
+  237  ls
+  238  touch leah.sh
+  239  ls
+  240  history > leah.sh
+  241  cat leah.sh
+  242  leah.fasta output
+  243  ls
+  244  cat leah.fasta
+  245  rm "grep -v '>' FOXP2.fasta |tr -dc 'C' | wc -c" >> leah.fasta
+  246  rm ~/leah.fasta/grep -v '>' FOXP2.fasta |tr -dc 'C'| wc -c
+  247  ~/
+  248  cd ~/leah.fasta
+  249  cd leah.fasta
+  250  cd ~/leah
+  251  ls
+  252  rm leah.fasta
+  253  ls
+  254  touch leah.fasta
+  255  ls
+  256  grep -v '>' FOXP2.fasta |tr -dc 'A'| wc -c
+  257  cat FOXP2.fasta
+  258  pwd
+  259  cd ~/home
+  260  cat FOXP2.fasta
+  261  cd ~/leah
+  262  ls
+  263  wget "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000007.14&rettype=FOXP2.fasta&retmode=text
+  264  cat leah.sh
+  265  wget "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&id=NC_000007.14&rettype=fasta&retmode=text" -OFOXP2.fasta
+  266  grep -v '>'FOXP2.fasta |tr -dc 'A'| wc -c
+  267  grep -v '>' FOXP2.fasta | tr -dc 'A' | wc -c
+  268  grep -v '>' FOXP2.fasta | tr -dc 'G' | wc -c
+  269  ls
+  270  cd ~/leah
+  271  ls
+  272  cat FOXP2.fasta
+  273  grep -v '>' FOXP2.fasta |tr -dc 'A'| wc -c
+  274  grep -v '>' FOXP2.fasta |tr -dc 'G'| wc -c
+  275  grep -v '>' FOXP2.fasta |tr -dc 'T'| wc -c
+  276  grep -v '>' FOXP2.fasta |tr -dc 'C'| wc -c
+  277  touch leah.fasta
+  278  ls
+  279  echo "Number of A = 47058248" >> leah.fasta
+  280  echo "Number of G = 32378859" >> leah.fasta
+  281  echo "Number of T = 47215040" >> leah.fasta
+  282  echo "Number of C = 32317984" >> leah.fasta
+  283  cat leah.fasta
+  284  cat leah.sh
+  285  history > leah.sh
