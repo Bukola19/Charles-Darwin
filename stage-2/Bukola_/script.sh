aea1d812da17f20105667349c69f47ed120319f9
Bukola19 <163158161+Bukola19@users.noreply.github.com>
2024-04-02 15:55:59 +0100

    Update script.sh

--- stage-2/Bukola_/script.sh
+++ stage-2/Bukola_/script.sh
@@ -4,44 +4,45 @@ mkdir PROJECT3 && cd PROJECT3
 # make new directories Data, Ref, results and change to the Data directory
 mkdir Data Ref Results && cd Data
 # Download the forward strand
-wget https://zenodo.org/records/10426436/files/ERR8774458_1.fastq.gz
-# Download the reverse strand
-wget https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz
+wget https://zenodo.org/records/10426436/files/ERR8774458_1.fastq.gz https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz
 # list the files and check the size
-ls-lh
+ls -lh
 # view the content of the zipped file
 zcat ERR8774458_1.fastq.gz | less -S
 # change the directory to the ref folder
-cd Ref
+cd .. && cd Ref
 # download the reference sequence
 wget https://zenodo.org/records/10886725/files/Reference.fasta
+cd ..
 # Activate the conda env
 conda activate base
 # Install fastqc
 conda install -c bioconda fastqc
 # change the directory to data
-cd PROJECT3/data/
+cd Data/
 # Run the quality control
-fastqc *.fastq.gz -o PROJECT/Results
+fastqc *.fastq.gz
+# Move the fastqc result to Results directory
+mv *_fastqc.zip *.html PROJECT3/Results/
+# List the files in the Results directory
+ls ~/PROJECT3/Results/
 # Install multiqc
 conda install -c bioconda multiqc
+# Change the directory to the result directory
+cd .. && cd Results
 # Run multiqc to aggregate the qc results
-multiqc *_fastq.gz > PROJECT3/results
+multiqc *_fastqc.zip
+# Change to the Data directory
+cd .. && cd Data
 # Trim the sequence
 fastp --thread 8 --detect_adapter_for_pe --cut_mean_quality 20 --length_required 50 --html report.html --json report.json -i ERR8774458_1.fastq.gz -I ERR8774458_2.fastq.gz -o trimmed_ERR8774458_1.fastq.gz -O trimmed_ERR8774458_2.fastq.gz
+# Move the Trimmed result to Results directory
+mv trimmed_ERR8774458_1.fastq.gz trimmed_ERR8774458_2.fastq.gz report.json report.html ~/PROJECT3/Results/
 conda install -c bioconda bwa
 # change the directory to Ref
-cd PROJECT/Ref/
+cd .. && cd Ref
 bwa index Reference.fasta 
-bwa mem Reference.fasta trimmed_ERR8774458_1.fastq.gz trimmed_ERR8774458_2.fastq.gz > /PROJECT3/results/mapped.sam
+bwa mem Reference.fasta ~/PROJECT3/Results/trimmed_ERR8774458_1.fastq.gz ~/PROJECT3/Results/trimmed_ERR8774458_2.fastq.gz > ~/PROJECT3/Results/mapped.sam
 cd ../
-cd results/
 conda install -c bioconda samtools
-samtools view -@ 20 -S -b /PROJECT3/results/mapped.sam > /PROJECT3/results/mapped.bam
-cd /PROJECT3/results/
-ls -lh
-samtools sort -@ 32 -o mapped_sorted.bam mapped.bam
-ls
-samtools index mapped_sorted.bam
-conda install -c bioconda bcftools
-
+cd .. && cd Results
