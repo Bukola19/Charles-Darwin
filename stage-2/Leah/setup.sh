ca0673537eb2366caf6f4c47f5e3d2579a6cba82
LeahIkubanni <163176382+LeahIkubanni@users.noreply.github.com>
2024-04-02 20:00:03 +0100

    Create setup.sh

---
+++ stage-2/Leah/setup.sh
@@ -0,0 +1,19 @@
+setup.sh
+#download and install anaconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# to create a working environment
+conda create -n variant-calling
+# to activate the environment
+conda activate variant-calling
+# to install fastqc for quality control
+conda install fastqc
+# to install multiqc for quality control( to aggregate fastqc result)
+conda install multiqc
+# to install bwa for genome mapping
+conda install bwa
+# to install samtools (to check the mapping statistics)
+conda install samtools
+# to install freebayes for variant calling
+conda install freebayes
+# to install bcftools for manipulation of vcf.gz file
+conda install bcftools
