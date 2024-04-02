e3c9ef05b8ba9de46556ba98a9472aee4077a8e8
tiamiyu1 <tiamiyuabdulwasiu@gmail.com>
2024-04-02 17:37:36 +0100

    task 1 completed and the pipeline works fine.

--- stage-2/tiamiyu/script.sh
+++ stage-2/tiamiyu/script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-# source ~/miniconda3/etc/profile.d/conda.sh
-# conda activate bioenv2
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate bioenv2
 
-echo  '==========Creating different directory=========='
-mkdir data data/fastq data/ref BAMS VCF
+echo  '==========Creating different directories=========='
+mkdir data data/fastq data/ref BAMS SAM VCF data/fastq/trimmed_reads
 
 echo  '==========Getting the data=========='
 wget -P data/fastq https://zenodo.org/records/10426436/files/ERR8774458_1.fastq.gz \
-		   https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz
+                   https://zenodo.org/records/10426436/files/ERR8774458_2.fastq.gz
 
 wget -P data/ref https://zenodo.org/records/10886725/files/Reference.fasta
 
@@ -17,47 +17,32 @@ fastqc data/fastq/*.fastq.gz
 multiqc data/fastq/*_fastqc.zip
 
 echo "==========Trimming with FastP=========="
+fastp  -i data/fastq/ERR8774458_1.fastq.gz -I data/fastq/ERR8774458_2.fastq.gz -o data/fastq/trimmed_reads/ERR8774458_1.fastq.gz -O data/fastq/trimmed_reads/ERR8774458_2.fastq.gz --html data/fastq/trimmed_reads/ERR8774458_fastp.html --json data/fastq/trimmed_reads/ERR8774458_fastp.json
 
 
+echo "==========Indexing the Refereence Genome with BWA=========="
 
-echo "==========Genome Mapping with bwa=========="
-bwa index data/ref/Reference.fasta
+bwa index -a bwtsw data/ref/Reference.fasta
+
+echo "==========Genome Mapping/Assembling with BWA=========="
+# to map the trimmed reads to a reference genome using BWA and producing SAM output
 
 ref=data/ref/Reference.fasta
-read1=data/fastq/ERR8774458_1.fastq.gz
-read2=data/fastq/ERR8774458_2.fastq.gz
+read1=data/fastq/trimmed_reads/ERR8774458_1.fastq.gz
+read2=data/fastq/trimmed_reads/ERR8774458_2.fastq.gz
 
-RGID="000"
-RGSN="ERR8774458"
-readgroup="@RG\\tID:${RGID}\\tPL:Illumina\\tPU:None\\tLB:None\\tSM:${RGSN}"
+bwa mem $ref $read1 $read2 > SAM/ERR8774458.sam
 
-bwa mem -t 8 -R "$readgroup" $ref $read1 $read2 | samtools view -h -b -o BAMS/ERR8774458.raw.bam
+# convert the SAM file into a BAM file that can be sorted and indexed
+samtools view -hbo BAMS/ERR8774458.bam SAM/ERR8774458.sam 
 
-samtools view -b -F 0xc BAMS/ERR8774458.raw.bam -o BAMS/ERR8774458.filtered.bam
+# sort the BAM file by position in genome
+samtools sort BAMS/ERR8774458.bam -o BAMS/ERR8774458.sorted.bam
 
-samtools sort -@ 8 -n BAMS/ERR8774458.filtered.bam -o BAMS/ERR8774458.sorted.n.bam
+# index the sorted BAM file to randomly access it quickly in variant calling 
+samtools index BAMS/ERR8774458.sorted.bam
 
-samtools fixmate -m BAMS/ERR8774458.sorted.n.bam BAMS/ERR8774458.fixmate.bam
+echo "==========Performing Variant Calling with bcftools=========="
+# to call variants from the mapped reads (sorted BAM) using a variant caller bcftools
 
-samtools sort -@ 8 BAMS/ERR8774458.fixmate.bam -o BAMS/ERR8774458.sorted.p.bam
-
-samtools markdup -r -@ 8 BAMS/ERR8774458.sorted.p.bam BAMS/ERR8774458.dedup.bam
-
-samtools index BAMS/ERR8774458.dedup.bam
-
-
-
-echo "==========Variant Calling with freebayes=========="
-freebayes -f data/ref/Reference.fasta -b BAMS/ERR8774458.dedup.bam --vcf VCF/ERR8774458.vcf
-
-bgzip VCF/ERR8774458.vcf
-
-bcftools index VCF/ERR8774458.vcf.gz
-
-
-echo "==========Variant Filtering=========="
-bcftools view -v snps VCF/ERR8774458.vcf.gz -Oz -o VCF/snps.vcf.gz
-
-bcftools filter -i "QUAL>=30" VCF/ERR8774458.vcf.gz -Oz -o VCF/variants-filtered.vcf.gz
-
-
+bcftools mpileup -Ou -f $ref BAMS/ERR8774458.sorted.bam|bcftools -Ov -mv > VCF/ERR8774458.vcf
--- stage-2/tiamiyu/setup.sh
+++ stage-2/tiamiyu/setup.sh
@@ -2,7 +2,7 @@
 conda config --add channels bioconda
 conda config --add channels conda-forge
 
-conda create -n bioenv2 fastqc multiqc bwa samtools freebayes bcftools tabix
+conda create -n bioenv2 fastqc fastp multiqc bwa samtools freebayes bcftools tabix
 
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate bioenv2
