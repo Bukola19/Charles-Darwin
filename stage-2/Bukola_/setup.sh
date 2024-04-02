e8d84ad54666975ce3a240b2b7b2e4c2eed633fd
Bukola19 <163158161+Bukola19@users.noreply.github.com>
2024-04-02 12:08:36 +0100

    Project3

---
+++ stage-2/Bukola_/setup.sh
@@ -0,0 +1,7 @@
+install conda
+conda install -c bioconda Fastqc
+conda install -c bioconda multiqc
+conda install -c bioconda Fastp
+conda install -c bioconda bwa
+conda install -c bioconda samtools
+conda install -c bioconda bcftools
