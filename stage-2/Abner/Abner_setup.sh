4dcf4922df6c58ce948019a2d252a5dceae771bd
Abner <162906308+FridayV@users.noreply.github.com>
2024-04-02 22:07:18 -0500

    Add files via upload

---
+++ stage-2/Abner/Abner_setup.sh
@@ -0,0 +1,29 @@
+# setup.sh
+
+# Install required tools
+
+# Activate environment
+conda activate funtools
+
+# Install FastQC
+sudo apt-get install fastqc
+
+# Install FastP
+sudo apt-get install fastp
+
+# Install BWA
+sudo apt-get install fastp
+
+# Install Samtools
+sudo apt-get install samtools
+
+# Install BCFtools
+sudo apt-get install bcftools
+
+#If there is a problem with libopenblas
+sudo apt-get install libopenblas-base
+#If the result is: E: Unable to locate package libopenblas-base 
+# so run the next command and then install libopenblas-base again:
+sudo apt-get update
+
+
