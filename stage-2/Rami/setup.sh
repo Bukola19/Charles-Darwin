53098f1c56a3eef698e9262809d4862a74d92189
Ramiaf <ramiaboufakher123@gmail.com>
2024-04-03 17:50:46 +0300

    remove setup.sh

--- stage-2/Rami/setup.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-conda install bioconda::fastqc
-conda install -c bioconda multiqc=1.21
-conda install bioconda::fastp
-conda install bioconda::bwa
-conda install bioconda::samtools
-conda install bioconda::bcftools
