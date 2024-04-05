54fa6ce5a041933eb346ca0ca61d721fbfac8281
LeahIkubanni <163176382+LeahIkubanni@users.noreply.github.com>
2024-04-05 09:49:00 +0100

    Update script.sh

--- stage-2/Leah/script.sh
+++ stage-2/Leah/script.sh
@@ -118,6 +118,176 @@ zgrep -v -c '^#' snps.vcf.gz
 bcftools filter -i "QUAL>=30" output.vcf.gz -oz -o variants.filtered.vcf.gz
 zgrep -v -c '^#' variants.filtered.vcf.gz
 
+Task2
+conda activate variant-calling
+  881  ls
+  882  cd stage2
+  888  mkdir project3
+  889  cd project3
+  890  mkdir downloadlinks.txt
+  891  cd downloadlinks.txt  ls
+                                                                                                                                                                        https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Alsen_R2.fastq.gz                                                                                      https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Baxter_R1.fastq.gz                                                                                     https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Baxter_R2.fastq.gz                                                                                     https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Chara_R1.fastq.gz                                                                                      https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Chara_R2.fastq.gz                                                                                      https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Drysdale_R1.fastq.gz                                                                                   https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Drysdale_R2.fastq.gz
+  936  echo "https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/ACBarrie_R1.fastq.gz
+  937  https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/ACBarrie_R2.fastq.gz                                                                                   https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Alsen_R1.fastq.gz
+  938  https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Alsen_R2.fastq.gz
+  939  https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Baxter_R1.fastq.gz
+  940  https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Baxter_R2.fastq.gz                                                                                     https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Chara_R1.fastq.gz
+  941  https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Chara_R2.fastq.gz
+  942  https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Drysdale_R1.fastq.gz
+  943  https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Drysdale_R2.fastq.gz" > download_links.txt
+  944  cat download_links.txt
+  
+
+  990  cat download_links.txt | xargs -i wget '{}'
+  991  ls
+  992  wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/ACBarrie_R2.fastq.gz
+  993  wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Alsen_R1.fastq.gz
+  994  wget https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/Chara_R1.fastq.gz
+  995  mkdir fastq2
+  996  mv *fastq*.gz. fastq2
+  997  mv *fastq.gz* fastq2
+  998  ls
+  999  cat fastq2
+ 1000  ls fastq2
+ 1001  mkdir ref
+ 1002  cd ref
+ 1003  wget https://raw.githubusercontent.com/josoga2/yt-dataset/main/dataset/raw_reads/reference.fasta
+ 1004  ls
+ 1005  cd ..
+ 1007  cd fastq2
+ 1008  ls
+ 1009  fastqc fastq2/*
+ 1010  fastqc
+ 1012  sudo apt install fastqc
+ 
+  fastqc fastq2/* fastq.gz
+ 1024  cd fastq2
+ 1025  ls
+ 1026  multiqc
+ 1029  conda install multiqc
+ 1030  multiqc
+ 1031  cd ..
+ 1032  multiqc fastq2/*fastqc*
+ 1033  ls
+ 
+ 1042  touch trim.sh
+ 1043  ls
+ 1044  nano trim.sh
+      !/bin/bash
+mkdir trimmed_reads
+SAMPLES=(
+  "ACBarrie"
+  "Alsen"
+  "Baxter"
+  "Chara"
+  "Drysdale"
+)
+
+for SAMPLE in "${SAMPLES[@]}"; do
+
+   fastp \
+     -i "$PWD/${SAMPLE}_R1.fastq.gz" \
+     -I "$PWD/${SAMPLE}_R2.fastq.gz" \
+     -o "trimmed_reads/${SAMPLE}_R1.fastq.gz" \
+     -O "trimmed_reads/${SAMPLE}_R2.fastq.gz" \
+     --html "trimmed_reads/${SAMPLE}_fastp.html"
+done
+ 1051  fastp
+ 1052  sudo apt install fastp
+ 1053  fastp
+ 1054  bash trim.sh
+ 1055  ls
+ 1056  ls trimmed_reads
+ 1057  cd trimmed_reads
+ 1058  ls
+
+ 1062  cd fastq2
+ 1063  fastqc trimmed_reads/* fastq.gz
+ 1064  cd trimmed_reads
+ 1065  ls
+
+
+ 1093  multiqc trimmed_reads/*fastqc*
+ 1094  cd trimmed_reads
+cd ..
+conda install bwa
+touch alignment_map
+conda install agbiome::bbtools
+nano alignment_map
+ #!/bin/bash
+     SAMPLES=(
+  "ACBarrie"
+  "Alsen"
+  "Baxter"
+  "Chara"
+  "Drysdale"
+)
+
+bwa index references/reference.fasta
+mkdir repair
+mkdir alignmentt_mapp
+
+for SAMPLE in "${SAMPLES[@]}"; do
+
+    repair.sh in1="trimmed_reads/${SAMPLE}_R1.fastq.gz" in2="trimmed_reads/${SAMPLE}_R2.fastq.gz" out1="repaired/${SAMPLE}_R1_rep.fastq.gz" out2="repaired/${SAMPLE}_R2_rep.fastq.gz" outsingle="repaired/${SAMPLE}_single.fq"
+    echo $PWD
+    bwa mem -t 1 \
+    references/reference.fasta \
+    "repaired/${SAMPLE}_R1_rep.fastq.gz" "repaired/${SAMPLE}_R2_rep.fastq.gz" \
+  | samtools view -b \
+  > "alignment_map/${SAMPLE}.sorted.bam"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
 
 
