5485930808918c0c897b48b2d054de0c4c627209
joy-adeoti <joyadeoti2@gmail.com>
2024-04-05 11:55:13 +0800

    Update joy_script.sh

--- stage-2/joy_stage2/joy_script.sh
+++ stage-2/joy_stage2/joy_script.sh
@@ -1,5 +1,5 @@
 
-task1
+#task1
 #pipeline for a pair read sample 
 #!/bin/bash
 source ~/anaconda3/etc/profile.d/conda.sh
@@ -50,12 +50,12 @@ samtools index BAMS/ERR8774458.sorted.bam
 bcftools mpileup -Ou -f $ref BAMS/ERR8774458.sorted.bam|bcftools call -Ov -mv > VCF/ERR8774458.vcf
 
 
-task2
+#task2
 
 #!/bin/bash
 #loop_script for ngs pipeline
 mkdir -p data
-data_dir="data"
+Data_dir="data"
 SAMPLES=(
     "ACBarrie"
     "Alsen"
@@ -69,36 +69,36 @@ for sample in "${SAMPLES[@]}";do
     read1_url="https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/${sample}_R1.fastq.gz"
     read2_url="https://github.com/josoga2/yt-dataset/raw/main/dataset/raw_reads/${sample}_R2.fastq.gz"
     
-    wget -P "${data_dir}" "$read1_url"
-    wget -P "${data_dir}" "$read2_url" 
+    wget -P "${Data_dir}" "$read1_url"
+    wget -P "${Data_dir}" "$read2_url" 
 done
 
 ref_url="https://raw.githubusercontent.com/josoga2/yt-dataset/main/dataset/raw_reads/reference.fasta"
-wget -P "${data_dir}" "$ref_url"
+wget -P "${Data_dir}" "$ref_url"
 
 #to perform the quality control using fastqc
 mkdir -p fastqc_out
-for fq in "${data_dir}"/*.fastq.gz;do
+for fq in "${Data_dir}"/*.fastq.gz;do
     #fastqc -i "${fq}" -o fastqc_out
     fastqc -f fastq -o fastqc_out "${fq}"
 done
 
 #to perform trimming using the fastp
 for fastq in "${SAMPLES[@]}";do
-    read1="${data_dir}/${fastq}_R1.fastq.gz"
-    read1_out="${data_dir}/${fastq}_R1.trimmed.fastq.gz"
-    read2="${data_dir}/${fastq}_R2.fastq.gz"
-    read2_out="${data_dir}/${fastq}_R2.trimmed.fastq.gz"
+    read1="${Data_dir}/${fastq}_R1.fastq.gz"
+    read1_out="${Data_dir}/${fastq}_R1.trimmed.fastq.gz"
+    read2="${Data_dir}/${fastq}_R2.fastq.gz"
+    read2_out="${Data_dir}/${fastq}_R2.trimmed.fastq.gz"
     fastp -i "${read1}" -o "${read1_out}" -I "${read2}" -O "${read2_out}"
 done
 
 #to perform genome mapping with the reference fasta using bwa
-reference="${data_dir}/reference.fasta"
+reference="${Data_dir}/reference.fasta"
 bwa index -a bwtsw "${reference}"
 mkdir -p bwa_outputfile
 for map in "${SAMPLES[@]}"; do
-    read1="${data_dir}/${map}_R1.trimmed.fastq.gz"
-    read2="${data_dir}/${map}_R2.fastq.gz"
+    read1="${Data_dir}/${map}_R1.trimmed.fastq.gz"
+    read2="${Data_dir}/${map}_R2.fastq.gz"
     bwa_result="bwa_outputfile/${map}.sam"
     bwa mem "${reference}" "${read1}" "${read2}" > "${bwa_result}"
     samtools view -hbo "bwa_outputfile/${map}.bam" "bwa_outputfile/${map}.sam"
